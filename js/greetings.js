2a62f384c7af4cea8b6805d9965b3970cc47e4a3
seominjeong <smj12938@gmail.com>
2022-05-18 17:01:41 +0900

    - todos 작업중

--- js/greetings.js
+++ js/greetings.js
@@ -1,111 +1,28 @@
-// // 3-4 event
-// const title = document.querySelector("h1");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-// function handleTitleCheck() {
-//     const currentColor = h1.style.color;
-//     let newColor;
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
-//     title.style.color = "red";
-//     if (currentColor === "red") {
-//         newColor = "tomato";
-//     } else {
-//         newColor = "blue";
-//     }
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
 
-//     h1.styled.color = newColor;
-// }
-// function handleMouseEnter() {
-//     title.innerText = "mouseenter다!";
-//     title.style.color = "red";
-// }
-// function handleMouseLeave() {
-//     title.innerText = "mouseLeave다!";
-//     title.style.color = "blue";
-// }
-// function handleWindowResize() {
-//     document.body.style.backgroundColor = "tomato";
-// }
-// function handleWindowCopy() {
-//     alert("Copier!");
-// }
-// function handleWindowOnline() {
-//     alert("Hello WIFI !");
-// }
-// function handleWindowOffline() {
-//     alert("SOS no WIFI !");
-// }
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
-// title.addEventListener("click", handleTitleCheck);
-// title.addEventListener("mouseover", handleMouseEnter);
-// title.addEventListener("mouseleave", handleMouseLeave);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-// window.addEventListener("resize", handleWindowResize);
-// window.addEventListener("copy", handleWindowCopy);
-// window.addEventListener("online", handleWindowOnline);
-// window.addEventListener("offline", handleWindowOffline);
-
-// 3-8
-// const h1 = document.querySelector("h1");
-
-// function handleTitleCheck() {
-//     const clickedClass = "clicked";
-
-//     h1.classList.toggle();
-// }
-
-// h1.addEventListener("click", handleTitleCheck);
-
-// 4-1
-// const loginForm = document.querySelector("#login-form");
-// const loginInput = loginForm.querySelector("#login-form input");
-// const loginButton = loginForm.querySelector("button");
-
-// function onLoginBtnClick() {
-//     const username = loginInput.value;
-//     if (username === "") {
-//         alert("Please write your name");
-//     } else if (username.length > 15) {
-//         alert("Your name is too long");
-//     }
-// }
-
-// 4-3
-// const link = document.querySelector("a");
-
-// function handleLinkClick(e) {
-//     e.preventDefault();
-//     alert("clicked!");
-// }
-
-// link.addEventListener("click", handleLinkClick);
-
-// 4-4
-// window.onload = function () {
-//     const loginForm = document.querySelector("#login-form");
-//     const loginInput = document.querySelector("#login-form input");
-//     const greeting = document.querySelector("#greeting");
-
-//     const HIDDEN_CLASSNAME = "hidden";
-
-//     function onLoginSubmit(event) {
-//         event.preventDefault();
-//         loginForm.classList.add(HIDDEN_CLASSNAME);
-//         const username = loginInput.value;
-//         paintGreetings(username);
-//     }
-
-//     function paintGreetings(username) {
-//         gretting.innerTExt = `Hello ${username}`;
-//         greeting.classList.remove(HIDDEN_CLASSNAME);
-//     }
-//     const savedUsername = localStorage.getItem(USERNAME_KEY);
-
-//     if (savedUsesrname === null) {
-//         loginForm.classList.remove(HIDDEN_CLASSNAME);
-//         loginForm.addEventListener("submit", onLoginSubmit);
-//     } else {
-//         paintGreetings();
-//     }
-
-//     loginForm.addEventListener("submit", onLoginSubmit);
-// };
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
---
+++ js/todo.js
@@ -0,0 +1,61 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+const TODOS_KEY = "todos";
+const toDos = [];
+
+// 로컬 스토리지에 투두 배열을 집어넣음.
+// 로컬 스토리지는 string format 밖에 지원을 안하기 때문.
+// stringify로 array처럼 생긴 string을 저장한 후 다시 parse를 이용해 array로 저장한것임.
+function saveToDos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteTodo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function paintTodo(newTodo) {
+    const li = document.createElement("li");
+    li.id = newTodo.id;
+    const span = document.createElement("span");
+    span.innerText = newTodo.text;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteTodo);
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
+function handleTodoSubmit(event) {
+    event.preventDefault();
+    // input의 현재 value를 새로운 변수에 복사.
+    // 이 변수는 input의 value를 비우기전의 값을 나타내는 string임.
+    // 그 다음 입력값을 paintTodo에 넣고 출력ㄴ
+    const newTodo = toDoInput.value;
+    toDoInput.value = "";
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(),
+    };
+    toDos.push(newTodoObj);
+    paintTodo(newTodoObj);
+    saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleTodoSubmit);
+
+function sayHello() {}
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+    // sting data type을 object로 만들어준것임.
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    // array에 있는 각각의 아이템에 실행해줌.
+    parsedToDos.forEach(paintTodo);
+}
